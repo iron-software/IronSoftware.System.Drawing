4c44d61c9547376388d306912be6c5e3cafeba59
Michael Blakie <michael@ironsoftware.com>
2023-02-22 11:45:15 +0700

    Remove unused reference

--- IronSoftware.Drawing/IronSoftware.Drawing.Common/AnyBitmap.cs
+++ IronSoftware.Drawing/IronSoftware.Drawing.Common/AnyBitmap.cs
@@ -5,7 +5,6 @@ using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
