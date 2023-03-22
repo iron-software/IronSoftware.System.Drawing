4a79826730ba649cbbef6019d52a4be8834752fd
Meee <104610177+mee-ironsoftware@users.noreply.github.com>
2023-03-22 11:09:00 +0700

    Change function name to Async
    Easy understand function is async test without having to look in the code
    
    Co-authored-by: Artem Koloskov <102773427+artem-iron@users.noreply.github.com>

--- IronSoftware.Drawing/IronSoftware.Drawing.Common.Tests/UnitTests/AnyBitmapFunctionality.cs
+++ IronSoftware.Drawing/IronSoftware.Drawing.Common.Tests/UnitTests/AnyBitmapFunctionality.cs
@@ -87,7 +87,7 @@ namespace IronSoftware.Drawing.Common.Tests.UnitTests
         }
 
         [FactWithAutomaticDisplayName]
-        public async void Create_AnyBitmap_by_Uri()
+        public async void Create_AnyBitmap_by_Uri_Async()
         {
             Uri uri = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1200px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg");
 
