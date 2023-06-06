e5897b4d9230008737ca3f8feec13c4fbd184a6f
michael-ironsoftware <97211369+michael-ironsoftware@users.noreply.github.com>
2023-06-06 16:30:39 +0700

    Add PointF and another test. More implicit casts

--- IronSoftware.Drawing/IronSoftware.Drawing.Common.Tests/UnitTests/PointFunctionality.cs
+++ IronSoftware.Drawing/IronSoftware.Drawing.Common.Tests/UnitTests/PointFunctionality.cs
@@ -15,7 +15,7 @@ namespace IronSoftware.Drawing.Common.Tests.UnitTests
         {
             int constructorX = 0;
             int constructorY = 0;
-            var pt = new Point(constructorX, constructorY);
+            Point pt = new Point(constructorX, constructorY);
             _ = pt.X.Should().Be(constructorX);
             _ = pt.Y.Should().Be(constructorY);
             _ = pt.Should().BeEquivalentTo(new Point(constructorX, constructorY));
@@ -32,6 +32,37 @@ namespace IronSoftware.Drawing.Common.Tests.UnitTests
             int xTranslation = 10;
             int yTranslation = -10;
             pt = new Point(constructorX, constructorY);
+            pt.Offset(xTranslation, yTranslation);
+            _ = pt.X.Should().Be(constructorX + xTranslation);
+            _ = pt.Y.Should().Be(constructorY + yTranslation);
+            _ = pt.Should().NotBeEquivalentTo(new Point(constructorX, constructorY));
+            _ = pt.Should().BeEquivalentTo(new Point(constructorX + xTranslation, constructorY + yTranslation));
+        }
+
+
+
+        [FactWithAutomaticDisplayName]
+        public void Create_new_PointF()
+        {
+            float constructorX = 0f;
+            float constructorY = 0f;
+            PointF pt = new PointF(constructorX, constructorY);
+            _ = pt.X.Should().Be(constructorX);
+            _ = pt.Y.Should().Be(constructorY);
+            _ = pt.Should().BeEquivalentTo(new Point(constructorX, constructorY));
+
+            constructorX = 20f;
+            constructorY = 5f;
+            pt = new PointF(constructorX, constructorY);
+            _ = pt.X.Should().Be(constructorX);
+            _ = pt.Y.Should().Be(constructorY);
+            _ = pt.Should().BeEquivalentTo(new Point(constructorX, constructorY));
+
+            constructorX = 20.5f;
+            constructorY = 5.33f;
+            float xTranslation = 10.88f;
+            float yTranslation = -10.32f;
+            pt = new PointF(constructorX, constructorY);
             pt.Offset(xTranslation, yTranslation);
             _ = pt.X.Should().Be(constructorX + xTranslation);
             _ = pt.Y.Should().Be(constructorY + yTranslation);
--- IronSoftware.Drawing/IronSoftware.Drawing.Common/PointF.cs
+++ IronSoftware.Drawing/IronSoftware.Drawing.Common/PointF.cs
@@ -3,36 +3,36 @@ using System;
 namespace IronSoftware.Drawing
 {
     /// <summary>
-    /// Represents an ordered pair of double x- and y-coordinates that defines a point in a two-dimensional plane.
+    /// Represents an ordered pair of integer x- and y-coordinates that defines a point in a two-dimensional plane.
     /// </summary>
-    public partial class Point
+    public partial class PointF
     {
         /// <summary>
-        /// Gets or sets the x-coordinate of this <see cref="IronSoftware.Drawing.Point"/>.
+        /// Gets or sets the x-coordinate of this <see cref="IronSoftware.Drawing.PointF"/>.
         /// </summary>
-        public double X { get; set; } = 0;
+        public float X { get; set; } = 0;
 
         /// <summary>
-        /// Gets or sets the y-coordinate of this <see cref="IronSoftware.Drawing.Point"/>.
+        /// Gets or sets the y-coordinate of this <see cref="IronSoftware.Drawing.PointF"/>.
         /// </summary>
-        public double Y { get; set; } = 0;
+        public float Y { get; set; } = 0;
         /// <summary>
-        /// Initializes a new instance of the <see cref="IronSoftware.Drawing.Point"/> struct with the specified coordinates.
+        /// Initializes a new instance of the <see cref="IronSoftware.Drawing.PointF"/> struct with the specified coordinates.
         /// </summary>
         /// <param name="x"></param>
         /// <param name="y"></param>
-        public Point(double x, double y)
+        public PointF(float x, float y)
         {
             X = x;
             Y = y;
         }
 
         /// <summary>
-        /// Translates this <see cref="IronSoftware.Drawing.Point"/> by the specified amount.
+        /// Translates this <see cref="IronSoftware.Drawing.PointF"/> by the specified amount.
         /// </summary>
         /// <param name="dx">The amount to offset the x-coordinate.</param>
         /// <param name="dy">The amount to offset the y-coordinate.</param>
-        public void Offset(double dx, double dy)
+        public void Offset(float dx, float dy)
         {
             X += dx;
             Y += dy;
@@ -41,23 +41,13 @@ namespace IronSoftware.Drawing
         #region Implicit Operators
 
         /// <summary>
-        /// Implicitly casts SixLabors.ImageSharp.Point objects to <see cref="Point"/>.  
+        /// Implicitly casts SixLabors.ImageSharp.PointF objects to <see cref="Point"/>.  
         /// <para>When your .NET Class methods use <see cref="Point"/> as parameters or return types, you now automatically support Points as well.</para>
         /// </summary>
-        /// <param name="point">System.Drawing.Point will automatically be cast to <see cref="Point"/> </param>
-        public static implicit operator Point(SixLabors.ImageSharp.Point point)
+        /// <param name="point">System.Drawing.PointF will automatically be cast to <see cref="Point"/> </param>
+        public static implicit operator PointF(SixLabors.ImageSharp.PointF point)
         {
-            return new Point(point.X, point.Y);
-        }
-
-        /// <summary>
-        /// Implicitly casts System.Drawing.Point objects to <see cref="Point"/>.  
-        /// <para>When your .NET Class methods use <see cref="Point"/> as parameters or return types, you now automatically support Points as well.</para>
-        /// </summary>
-        /// <param name="point">System.Drawing.Point will automatically be cast to <see cref="Point"/> </param>
-        public static implicit operator Point(System.Drawing.Point point)
-        {
-            return new Point(point.X, point.Y);
+            return new PointF(point.X, point.Y);
         }
 
         /// <summary>
@@ -65,23 +55,25 @@ namespace IronSoftware.Drawing
         /// <para>When your .NET Class methods use <see cref="Point"/> as parameters or return types, you now automatically support Points as well.</para>
         /// </summary>
         /// <param name="point">Microsoft.Maui.Graphics.Point will automatically be cast to <see cref="Point"/> </param>
-        public static implicit operator Point(Microsoft.Maui.Graphics.Point point)
+        public static implicit operator PointF(Microsoft.Maui.Graphics.PointF point)
         {
-            return new Point(point.X, point.Y);
+            return new PointF(point.X, point.Y);
         }
 
         /// <summary>
-        /// Implicitly casts SkiaSharp.SKPointI objects to <see cref="Point"/>.  
+        /// Implicitly casts SkiaSharp.SKPoint objects to <see cref="Point"/>.  
         /// <para>When your .NET Class methods use <see cref="Point"/> as parameters or return types, you now automatically support Points as well.</para>
         /// </summary>
-        /// <param name="point">SkiaSharp.SKPointI will automatically be cast to <see cref="Point"/> </param>
-        public static implicit operator Point(SkiaSharp.SKPointI point)
+        /// <param name="point">SkiaSharp.SKPoint will automatically be cast to <see cref="Point"/> </param>
+        public static implicit operator PointF(SkiaSharp.SKPoint point)
         {
-            return new Point(point.X, point.Y);
+            return new PointF(point.X, point.Y);
         }
+
         #endregion
 
         #region Override Methods
+
         /// <summary>
         /// Specifies whether this <see cref="IronSoftware.Drawing.Point"/> instance contains the same coordinates as another <see cref="IronSoftware.Drawing.Point"/>.
         /// </summary>
