bc82d6b3c5c42c0970f32e6ad5f51550f11a5c5e
michael-ironsoftware <97211369+michael-ironsoftware@users.noreply.github.com>
2023-10-15 09:47:03 +0700

    Deprecate CropRectangle with Obsolete message

--- IronSoftware.Drawing/IronSoftware.Drawing.Common/Legacy/CropRectangle.cs
+++ IronSoftware.Drawing/IronSoftware.Drawing.Common/Legacy/CropRectangle.cs
@@ -1,12 +1,18 @@
 using System;
+using System.ComponentModel;
 
 namespace IronSoftware.Drawing
 {
-    /// <summary>
-    /// A universally compatible Rectangle for .NET 7, .NET 6, .NET 5, and .NET Core. As well as compatiblity with Windows, NanoServer, IIS, macOS, Mobile, Xamarin, iOS, Android, Google Compute, Azure, AWS, and Linux.
+
+    /// A universally compatible Rectangle for .NET 7, .NET 6, .NET 5, and .NET Core. As well as compatibility with Windows, NanoServer, IIS, macOS, Mobile, Xamarin, iOS, Android, Google Compute, Azure, AWS, and Linux.
     /// <para>Works nicely with popular Image Rectangle such as System.Drawing.Rectangle, SkiaSharp.SKRect, SixLabors.ImageSharp.Rectangle, Microsoft.Maui.Graphics.Rect.</para>
-    /// <para>Implicit casting means that using this class to input and output Rectangle from public API's gives full compatibility to all Rectangle type fully supported by Microsoft.</para>
+    /// <para>Implicit casting means that using this class to input and output Rectangle from public APIs gives full compatibility to all Rectangle type fully supported by Microsoft.</para>
+    /// <para>Legacy support.</para>
     /// </summary>
+    [Browsable(false)]
+    [Bindable(false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [Obsolete("Please use Rectangle instead of CropRectangle", false)]
     public class CropRectangle
     {
         /// <summary>
