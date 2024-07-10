f69f3015289e003612945a7154c07aa5a4c77bd2
Bline <115607429+bline-ironsoftware@users.noreply.github.com>
2024-07-10 10:21:17 +0700

    Fixes casting issue from AnyBitmap to SKBitmap on Linux

--- IronSoftware.Drawing/IronSoftware.Drawing.Common.ConsoleTest/Program.cs
+++ IronSoftware.Drawing/IronSoftware.Drawing.Common.ConsoleTest/Program.cs
@@ -1,12 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using IronSoftware.Drawing;
+using SkiaSharp;
 
-ReadOnlySpan<byte> bytes = File.ReadAllBytes("test.bmp");
+//ReadOnlySpan<byte> bytes = File.ReadAllBytes("test.bmp");
 
-for (int i=0; i<10000; i++)
-{
-    using AnyBitmap bmp = new AnyBitmap(bytes);
-    var bin = bmp.GetBytes();
-    Console.WriteLine(bin.Length);
-}
+//for (int i=0; i<10000; i++)
+//{
+//    using AnyBitmap bmp = new AnyBitmap(bytes);
+//    var bin = bmp.GetBytes();
+//    Console.WriteLine(bin.Length);
+//}
+
+AnyBitmap bmp = new AnyBitmap(100, 100);
+SKBitmap skbmp = bmp;
+Console.WriteLine("Done!");
